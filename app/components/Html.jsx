d8a538776e58f2f91a7725e76d351d3b01eb6037
Akash Mulye <akashmulye97@gmail.com>
2024-03-15 01:29:52 -0400

    home page animations and styling changes

--- app/components/Html.jsx
+++ app/components/Html.jsx
@@ -1,39 +1,52 @@
+import ARROW from '../assets/letsgo-arrow.svg';
+
 export const Html = () => {
     return (
         <>
           <h1
             style={{
               position: 'absolute',
-              top: '35vh',
-              left: '50vw',
-              transform: 'translateX(-50%)',
+              top: '20vh',
+              left: '60vw',
+              // transform: 'translateX(-50%)',
               color: '#fff',
-              fontSize: '6em'
+              fontSize: '1.2em'
             }}>
             Welcome to the astroverse
+            <div className='dot-container'><span className='inner-dot'></span></div>
           </h1>
           <h1
             style={{
               position: 'absolute',
-              top: '120vh',
+              top: '100vh',
               left: '50vw',
               transform: 'translateX(-65%)',
               color: '#fff',
-              fontSize: '6em'
+              fontSize: '1.2em'
             }}>
+              <div className='dot-container'><span className='inner-dot'></span></div>
             We're on a mission
           </h1>
           <h1
             style={{
               position: 'absolute',
-              top: '210vh',
+              top: '200vh',
               left: '50vw',
               transform: 'translateX(-50%)',
               color: '#fff',
-              fontSize: '6em'
+              fontSize: '1.2em'
             }}>
-            To help you find your perfect high
+            To help you find your perfect high<div className='dot-container'><span className='inner-dot'></span></div>
           </h1>
+          <img
+            style={{
+              position: 'absolute',
+              top: '270vh',
+              left: '50vw',
+              transform: 'translateX(-50%)',
+              color: '#fff',
+              fontSize: '1.2em'
+            }} src={ARROW} />
         </>
     )  
 }
